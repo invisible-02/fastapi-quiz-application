65fa4031535683c0e65625e2fb285b4313dd17e6
Shivam Bhardwaj <114326888+invisible-02@users.noreply.github.com>
2025-06-06 12:49:57 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -22,7 +22,15 @@ async def init():
                     query = questions.insert().values(
                         id=q["id"],
                         question_text=q["question"],
-                        options=options_json
+                        options=options_json,
+                        req_runs=q.get("req_runs"),
+                        balls_remaining=q.get("balls_remaining"),
+                        batsman_total_runs=q.get("batsman_total_runs"),
+                        batsman_balls_faced=q.get("batsman_balls_faced"),
+                        nonstriker_total_runs=q.get("nonstriker_total_runs"),
+                        nonstriker_balls_faced=q.get("nonstriker_balls_faced"),
+                        team_run_rate=q.get("team_run_rate"),
+                        wickets=q.get("wickets"),
                     )
                     await database.execute(query)
     await database.disconnect()
